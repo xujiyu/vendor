eaad63673d25b44fe8c39af50cc8c38d142f70b4
ccorbin17 <carinabcorbin@gmail.com>
2021-05-06 21:03:16 -0400

    added editing feature but update feature not ready

--- frontend/my-app/src/ShopPage.js
+++ frontend/my-app/src/ShopPage.js
@@ -1,6 +1,8 @@
 import React, { useMemo, useState, useEffect , Component} from "react";
 import { render } from 'react-dom';
 import ReactTable from 'react-table';
+import { useForm, useField, splitFormProps } from "react-form";
+import { useTable } from "react-table";
 import logo from './restoration-logo.png';
 import us from './vendor-logo.PNG';
 
@@ -16,6 +18,20 @@ function ShopPage(props) {
       
         );
       };
+      const NumericalInput = props => {
+        console.log("NumericalInput", props);
+        const { column, row, cell, updateData } = props;
+        const onChange = e => updateData(row.index, column.id, e.target.value);
+        return <input type="number" value={cell.value} onChange={onChange} />;
+      };
+ 
+      const StringInput = props => {
+        console.log("StringInput", props);
+        const { column, row, cell, updateData } = props;
+        const onChange = e => updateData(row.index, column.id, e.target.value);
+        return <input type="string" value={cell.value} onChange={onChange} />;
+      };
+
       const columns = useMemo(
         () => [
     
@@ -26,19 +42,23 @@ function ShopPage(props) {
             columns: [
               {
                 Header: "Item",
-                accessor: "item.name"
+                accessor: "item.name",
+                Cell: StringInput
               },
               {
                 Header: "Type",
-                accessor: "item.type"
+                accessor: "item.type",
+                Cell: StringInput
               },
               {
                 Header: "QTY",
-                accessor: "item.quantity"
+                accessor: "item.quantity",
+                Cell: NumericalInput
               },
               {
                 Header:"Unit Price",
-                accessor:"item.price"
+                accessor:"item.price",
+                Cell: StringInput
               }
             ]
           },
@@ -50,11 +70,13 @@ function ShopPage(props) {
               {
                 Header: "Order(s)",
                 accessor: "item.orders",
+                Cell: NumericalInput
                 // Cell: ({ cell: { value } }) => <Orders values={value} />
               },
               {
                 Header: "Status",
                 accessor: "item.status",
+                Cell: StringInput
               //   Cell: row => {
               //     row.styles["backgroundColor"] = row.value == "In Progress" ? "red" : "green";
               //     return row.value.toUpperCase();
@@ -62,7 +84,8 @@ function ShopPage(props) {
               },
               {
                 Header: "Last Updated",
-                accessor: "item.update"
+                accessor: "item.update",
+                Cell: StringInput
               }
     
             ]
@@ -71,7 +94,7 @@ function ShopPage(props) {
         []
       );
     
-      const data = [
+      const initialData = [
         {
           "item": {
             "id": 44813,
@@ -133,12 +156,50 @@ function ShopPage(props) {
             }
         }
       ]
+      const [data, setData] = React.useState(initialData);
+      const resetData = () => setData(initialData);
+      const addRow = () => setData(old => [...old, { id: 33555, name: "Madagascar Vanilla Extract", type: "Pantry", quantity: 10, orders: 0, status: "Not Applicable", update: "5/6/2021", price:"$8.40"}]);
+      const updateData = (rowIndex, columnID, value) => {
+        setData(oldData =>
+          oldData.map((row, index) => {
+            if (index === rowIndex) {
+              return {
+                ...oldData[rowIndex],
+                [columnID]: value
+              };
+            }
+            return row;
+          })
+        );
+      };
+      const table = useTable({ columns, data, updateData });
+      const { getTableProps, headerGroups, rows, prepareRow } = table;
+    //   const tableSum = rows.reduce((sum, row) => sum + row.values.total, 0);
+    //   console.log("setAmountDue", tableSum);
+    //   setAmountDue(tableSum);
     return(
         <div className="Shop">
         <img src={logo} width="150" height="100"/>
      
         <center> <h1>Welcome Back </h1> <h1color> Restoration Farms </h1color> </center>
+        <table> 
         <Table columns={columns} data={data} />
+        <tbody>
+          <tr>
+            <td colSpan={3}>
+              <button type="button" onClick={addRow}>
+                Add Row
+              </button>
+              <button type="button" onClick={resetData}>
+                Revert To Last Save
+              </button>
+            </td>
+          </tr>
+        </tbody>
+
+
+        </table>
+
         <br></br>
   
         <br></br>
